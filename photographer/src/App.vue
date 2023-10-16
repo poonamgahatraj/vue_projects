8c06c291f66656bbf9dff0ca08a7a9933d09f08f
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-16 14:22:27 +0545

    photographer

---
+++ photographer/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import firstpage from './pages/firstpage.vue';
+</script>
+
+<template>
+  <firstpage/>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ photographer/src/pages/firstpage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="image">
+        <img src="public/assets/photographer.jpg" style="height:400px;width:100%">
+
+        <div class="text">
+            <h1 style="font-size:50px">I am John Doe</h1>
+            <p>And I'm a Photographer</p>
+            <button>Hire me</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.image{
+    width:100vw;
+    background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("photographer.jpg");
+}
+
+.text{
+    position: absolute;
+    top: 20%;
+    right: 40%;
+    color: white;
+    text-align: center;
+    width: 20%;
+}
+</style>
